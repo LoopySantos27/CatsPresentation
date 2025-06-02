1d4624c49c1385a67893d26d1c0e91a973904862
Gerardo Santos <gerardodsc27@gmail.com>
2025-06-02 10:29:24 -0600

    FirstPush

---
+++ 3D_Website/vite.config.js
@@ -0,0 +1,19 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), sentryVitePlugin({
+    org: "jsm-sr",
+    project: "javascript-react"
+  })],
+
+  optimizeDeps: {
+    exclude: ['js-big-decimal']
+  },
+
+  build: {
+    sourcemap: true
+  }
+})
